c3dddc759aae17f6fcebdbe96368477d011dd5e8
Jonathan Delgado <hi@jon.soy>
2025-05-15 17:56:56 -0400

    feat: add info and warning logging functions to utils.sh

--- utils/utils.sh
+++ utils/utils.sh
@@ -3,6 +3,8 @@
 
 DEBUG_PREFIX="\033[1;34m[DEBUG]\033[0m"
 ERROR_PREFIX="\033[1;31m[ERROR]\033[0m"
+INFO_PREFIX="\033[1;32m[INFO]\033[0m"
+WARNING_PREFIX="\033[1;33m[WARNING]\033[0m"
 
 function echo_debug() {
     echo "${DEBUG_PREFIX} $1"
@@ -10,4 +12,12 @@ function echo_debug() {
 
 function echo_error() {
     echo "${ERROR_PREFIX} $1"
+}
+
+function echo_info() {
+    echo "${INFO_PREFIX} $1"
+}
+
+function echo_warning() {
+    echo "${WARNING_PREFIX} $1"
 }
